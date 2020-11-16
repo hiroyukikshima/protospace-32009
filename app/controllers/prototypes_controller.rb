edfb451a0c8514d04cb042c7c9e922a5c038d2a4
kwsmhryk <hryk0619@gmail.com>
2020-11-16 14:37:59 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,61 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @prototypes = Prototype.all
+    @current_user = User.find(current_user.id)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      @prototype = Prototype.find(params[:id])
+      render "prototypes/edit"
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:image, :concept, :catch_copy, :title).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    if user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+end
